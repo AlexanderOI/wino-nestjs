310fef362e49e7213e003b41d5da17ce1c45c541
AlexanderOI <vialexoriba1257@gmail.com>
2025-07-13 22:45:19 -0300

    refactor: remove unnecessary string validation from AssignFormTaskDto to streamline project ID assignment

--- src/forms-task/dto/request/assign-form-task.dto.ts
+++ src/forms-task/dto/request/assign-form-task.dto.ts
@@ -1,13 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Transform } from 'class-transformer'
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsNotEmpty } from 'class-validator'
 import { Types } from 'mongoose'
 
 export class AssignFormTaskDto {
   @ApiProperty({
     description: 'The ID of the project to assign the form task to',
   })
-  @IsString()
   @IsNotEmpty()
   @Transform(({ value }) => new Types.ObjectId(value))
   projectId: Types.ObjectId
